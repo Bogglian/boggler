675c795b4bf51083fb3db255e2528d93821bb5c3
Rayleigh <gmj1197@gmail.com>
2018-12-30 17:38:39 +0900

    feat: Add component style for voice wave

---
+++ src/styledComponents/StyledPlayback/index.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SPlayback = styled.div`
+  & {
+    .play-pause-btn {
+      cursor: pointer;
+      width: 100%;
+      height: 100%;
+      padding: 10%;
+      text-align: center;
+    }
+`;
+
+export { SPlayback };
