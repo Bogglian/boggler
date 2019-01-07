491cff00c1d6f74398925bc52239b14a46fe06d7
Rayleigh <gmj1197@gmail.com>
2019-01-07 16:54:33 +0900

    feat: Add audio wave

--- src/components/Wave/index.js
+++ src/components/Wave/index.js
@@ -1,19 +1,43 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import Waveform from "waveform-react";
+
 import { VoiceWave, WaveLayout } from "../../styledComponents";
 
-const Wave = ({ audioPath, onBuffer, onReady, onPlay }) => {
+const Wave = ({
+  audioPath,
+  onBuffer,
+  onReady,
+  onPlay,
+  onPosChange,
+  buffer,
+  markerStyle,
+  position,
+  waveStyle
+}) => {
   return (
     <WaveLayout>
       <VoiceWave>
-        <ReactPlayer
-          url={audioPath}
+        <Waveform
           width="100%"
-          height="100%"
+          height="50%"
+          onPositionChange={onPosChange}
+          buffer={buffer}
+          position={position}
+          markerStyle={markerStyle}
+          waveStyle={waveStyle}
+          responsive
+          showPosition
+        />
+        <ReactPlayer
+          width="100%"
+          height="50%"
           onBuffer={onBuffer}
           onReady={onReady}
           onPlay={onPlay}
+          url={audioPath}
           playing
+          controls
         />
       </VoiceWave>
     </WaveLayout>
--- src/containers/AudioContainer.js
+++ src/containers/AudioContainer.js
@@ -9,12 +9,23 @@ import {
   Positioner,
   ShadowedBox
 } from "../styledComponents";
+import { getAudioBuffer, getContext } from "../lib/utils";
 
 import * as postingActions from "../store/modules/posting";
 import * as audioActions from "../store/modules/audio";
 import * as api from "../lib/api";
 
 class AudioContainer extends Component {
+  componentWillMount() {
+    const { AudioActions } = this.props;
+    const audioContext = getContext();
+
+    // setContext 액션함수 추가 필요
+    AudioActions.setContext({
+      context: audioContext
+    });
+  }
+
   handleBuffer = () => {
     const { PostingActions } = this.props;
 
@@ -34,25 +45,34 @@ class AudioContainer extends Component {
       value
     });
   };
+
   handleClickBold = () => {};
   handleClickHeader = () => {};
   handleClickItelic = () => {};
   handleClickQuote = () => {};
+
   handleClickEdit = () => {
     const { PostingActions } = this.props;
 
     PostingActions.editorOn();
   };
+
   handleReady = () => {
     const { PostingActions } = this.props;
 
     PostingActions.bufferMedia();
   };
+
   handleUploadFile = e => {
     const { AudioActions } = this.props;
+    const fileURL = window.URL.createObjectURL(e.target.files[0]);
 
-    AudioActions.uploadFile({ file: e.target.files[0] });
+    this.getFile(fileURL);
+
+    // uploadFile 액션함수 수정 필요( 인자 2개로 )
+    AudioActions.uploadFile({ file: e.target.files[0], url: fileURL });
   };
+
   //액션함수를 호출하면 렌더가 계속 됌
   handleClickSave = () => {
     const { PostingActions, id } = this.props;
@@ -64,6 +84,21 @@ class AudioContainer extends Component {
     console.log("New Posts");
     this.createPosts();
     PostingActions.bufferDone();
+  };
+
+  handlePosChange = pos => {
+    const { AudioActions } = this.props;
+
+    // setPosition 액션함수 필요
+    AudioActions.setPosition({ markerPosition: pos });
+  };
+
+  getFile = async (path = "audio/test.mp3") => {
+    const { AudioActions, context } = this.props;
+    const fileBuffer = await getAudioBuffer(path, context);
+
+    // uploadBuffer 액션함수 필요
+    AudioActions.uploadBuffer(fileBuffer);
   };
 
   modifyPosts = async () => {
@@ -110,11 +145,19 @@ class AudioContainer extends Component {
       PostingActions.editorOn();
     });
   };
-  render() {
-    const { buffering, editorMode, title, content, file } = this.props;
 
-    console.log("Hell" + content);
-    const filePath = "https://www.youtube.com/watch?v=YBzJ0jmHv-4";
+  render() {
+    const {
+      buffering,
+      editorMode,
+      title,
+      content,
+      file,
+      url,
+      buffer,
+      markerPosition
+    } = this.props;
+
     return (
       <Positioner clasName="audio">
         <AudioProgressbar className={buffering ? "" : "none"} />
@@ -122,10 +165,23 @@ class AudioContainer extends Component {
           <ShadowedBox>
             <div className="voicewave-box">
               <Wave
-                audioPath={filePath}
+                audioPath={url}
                 onBuffer={this.handleBuffer}
                 onReady={this.handleReady}
                 onPlay={this.handlePlay}
+                onPosChange={pos => this.handlePosChange(pos)}
+                buffer={buffer}
+                position={markerPosition}
+                markerStyle={{
+                  color: "#030303",
+                  width: 4
+                }}
+                waveStyle={{
+                  animate: true,
+                  color: "#020202",
+                  plot: "bar",
+                  pointWidth: 1
+                }}
               />
             </div>
           </ShadowedBox>
@@ -161,7 +217,9 @@ const mapStateToProps = ({ posting, audio }) => ({
   id: audio.id,
   title: audio.title,
   content: audio.content,
+  context: audio.context,
   file: audio.file,
+  markerPosition: audio.markerPosition,
   editorMode: posting.editorMode,
   buffering: posting.buffering
 });
