481c5e34545a4edd509870423fe206d0b739b5fe
jihun <hojihun5516@daum.net>
2019-01-07 15:25:05 +0900

    feat semicolons
    Add a semicolon at the end of a statement

--- src/server/router/board.js
+++ src/server/router/board.js
@@ -5,8 +5,8 @@ const board = require('../controller/boardController')()
 
 router.post('', board.write)
 router.get('', board.showAll)
-router.get('/:boardId',board.showOne)
+router.get('/:boardId', board.showOne)
 router.delete('/:boardId', board.delete)
-router.put('/:boardId',board.modify)
+router.put('/:boardId', board.modify)
 
 module.exports = router;
