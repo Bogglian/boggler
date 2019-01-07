e502f47b4a9147d96b3d1b31afb1ca17541ff769
jihun <hojihun5516@daum.net>
2019-01-07 15:23:26 +0900

    feat upload new audio file path
    -file path to public/upload

--- src/server/controller/boardController.js
+++ src/server/controller/boardController.js
@@ -1,98 +1,102 @@
-const board = require('../db/board')()
-const file = require('../db/file')()
-const uniqueFilename = require('unique-filename')
-const ds = require('../../lib/deepSpeech')()
+const board = require("../db/board")();
+const file = require("../db/file")();
+const uniqueFilename = require("unique-filename");
+const ds = require("../../lib/deepSpeech")();
 
-module.exports= function() {
-    return{
-       modify : function(req,res,next){
-            board.modify(req.params.boardId,req.body,function(err,data){
-                if(err){
-                    next(err)
+module.exports = function () {
+    return {
+        modify: function (req, res, next) {
+            board.modify(req.params.boardId, req.body, function (err, data) {
+                if (err) {
+                    next(err);
                 }
-                res.status(200).json({success : true})
-            })
+                res.status(200).json({ success: true });
+            });
         },
-        delete : function(req,res,next){
-            board.delete(req.params.boardId,function(err,data){
-                if(err){
-                    next(err)
+        delete: function (req, res, next) {
+            board.delete(req.params.boardId, function (err, data) {
+                if (err) {
+                    next(err);
                 }
-                res.status(200).json({success: true})
-            })
+                res.status(200).json({ success: true });
+            });
         },
-        showAll : function(req,res,next){
-            board.showAll(function(err,data){
-                if(err){
-                    next(err)
+        showAll: function (req, res, next) {
+            board.showAll(function (err, data) {
+                if (err) {
+                    next(err);
                 }
-                res.status(201).json({boards:data})
-            })
+                res.status(201).json({ boards: data });
+            });
         },
-        showOne : function(req,res,next){
-            board.showOne(req.params.boardId,function(err,data){
-                if(err){
-                    next(err)
+        showOne: function (req, res, next) {
+            board.showOne(req.params.boardId, function (err, data) {
+                if (err) {
+                    next(err);
                 }
-                res.status(201).json({board:data[0]})
-            })
+                res.status(201).json({ board: data[0] });
+            });
         },
-        write : function(req,res,next) {
-            var body = new Object()
-            body.title=req.body.title
-            if(!req.files){
-                body.content=req.body.content
-                console.log('just write board no file')
-                board.write(body,function(err,writeResult){
-                    if(err){
-                        next(err)
+        write: function (req, res, next) {
+            var body = new Object();
+            body.title = req.body.title;
+            if (!req.files) {
+                body.content = req.body.content;
+                console.log("just write board no file");
+                board.write(body, function (err, writeResult) {
+                    if (err) {
+                        next(err);
                     }
-                     board.showOne(writeResult.insertId,function(err,showResult){
-                        if(err){
-                            next(err)
+                    board.showOne(writeResult.insertId, function (err, showResult) {
+                        if (err) {
+                            next(err);
                         }
-                        res.status(201).json({board:showResult[0]})
-                    })
-                })
-             }else{
-                
-                console.log(`write board with file filename: ${req.files.audiofile.name}`)
-                let getFile = req.files.audiofile
+                        res.status(201).json({ board: showResult[0] });
+                    });
+                });
+            } else {
+                console.log(
+                    `write board with file filename: ${req.files.audiofile.name}`
+                );
+                let getFile = req.files.audiofile;
                 //file뭘로 받을지 작성
-                let fakeName = uniqueFilename('')
+                let fakeName = uniqueFilename("");
                 console.log(fakeName);
-                getFile.mv(`${__dirname}/../upload/${fakeName}`,function(err){
-                    
-                    if(err){
-                        next(err)
+                getFile.mv(`${__dirname}/../../../public/upload/${fakeName}`, function (
+                    err
+                ) {
+                    if (err) {
+                        next(err);
                     }
-                    ds(fakeName,function(err,sttResult){
-                        if(err){
-                            next(err)
+                    ds(fakeName, function (err, sttResult) {
+                        if (err) {
+                            next(err);
                         }
-                        body.content=sttResult
-                        console.log(`boardController의 stt ${body.content}`)
-                    
-                        board.write(body,function(err,writeResult){
-                        if(err){
-                            next(err)
-                        }
-                        file.upload(getFile.name,fakeName,writeResult.insertId,function(err,fileResult){
-                            if(err){
-                                next(err)
+                        body.content = sttResult;
+                        board.write(body, function (err, writeResult) {
+                            if (err) {
+                                next(err);
                             }
-
-                            board.showOne(writeResult.insertId,function(error,data){
-                                if(error){
-                                    next(error)
+                            file.upload(
+                                getFile.name,
+                                fakeName,
+                                writeResult.insertId,
+                                function (err, fileResult) {
+                                    if (err) {
+                                        next(err);
+                                    }
+                                    board.showOne(writeResult.insertId, function (error, data) {
+                                        if (error) {
+                                            next(error);
+                                        }
+                                        res.status(201).json({ board: data[0] });
+                                    });
                                 }
-                                res.status(201).json({board:data[0]})
-                            })
-                        })
-                    })
-                })
-                })
+                            );
+                        });
+                    });
+                });
             }
         }
-    }
-}
+    };
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,39 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const fileUpload = require("express-fileupload")
-const board = require('./router/board')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const board = require("./router/board");
+const app = express();
 
 //middleware
-app.use(bodyParser.json())
-app.use(fileUpload())
+app.use(bodyParser.json());
+app.use(fileUpload());
+app.use(express.static(__dirname + "/../../public"));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
-
+  );
+  next();
+});
 
 //router
-app.use("/board", board)
+app.use("/board", board);
 
 //next함수
 app.use(function(err, req, res, next) {
-    console.log(`error occurrence : ${err}`)
-    if (!err.statusCode) {
-      err.statusCode = 500
-    }
-    res.status(err.statusCode).json({
-      errorMassage: err,
-      success: false
-    })
-  })
+  console.log(`error occurrence : ${err}`);
+  if (!err.statusCode) {
+    err.statusCode = 500;
+  }
+  res.status(err.statusCode).json({
+    errorMassage: err,
+    success: false
+  });
+});
 
-app.listen(8080,function(){
-    console.log('server on port 8080')
-})
+app.listen(8080, function() {
+  console.log("server on port 8080");
+});
