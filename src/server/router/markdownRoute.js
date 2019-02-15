fdf2b59a29de27dd8b31aaa9aa27c70e481c24bf
KangGilHwan <rlfghksop@naver.com>
2019-02-15 16:23:30 +0900

    feat: Add convert markdown to pdf file for user

---
+++ src/server/router/markdownRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/pdf', function(req, res) {
+  console.log(`[Convert markdown to pdf] Text : ${req.body.markdownText});
+  markdownpdf().from.string(req.body.markdownText).to.buffer("", function (err,buffer) {
+  res.send(buffer);
+})
+
+module.exports = router;
